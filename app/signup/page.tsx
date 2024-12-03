95f871a76002c9d99069a8bf1135242b4b9107f5
24HP6902 24HP6902 <24HP6902@administrator.local>
2024-12-03 15:45:40 +0800

    instagram

---
+++ app/signup/page.tsx
@@ -0,0 +1,91 @@
+"use client";
+
+import { Input } from "@/components/ui/input";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+  CardFooter,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { ChangeEvent, useState } from "react";
+
+export default function Page() {
+  const [email, setEmail] = useState<string>("");
+  const [username, setUserName] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmailValue = (e: ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
+  // const signUp = () => {
+  //   if (email === "") {
+  //     setEmailError(true);
+  //   } else {
+  //     setEmailError(false);
+  //   }
+  // };
+
+  const eMail = () => {};
+
+  return (
+    <div className="flex justify-center items-center bg-black h-screen">
+      <Card className="flex flex-col w-[350px] bg-black border-none">
+        <CardHeader>
+          <CardTitle className="text-5xl font text-white font-black justify-center mb-10">
+            Instagram
+          </CardTitle>
+          <CardDescription className="text-xl font text-white font-bold">
+            Signup to see photos and videos from your friends.
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="grid gap-y-3 ">
+          <Input
+            placeholder="firstName"
+            className="border-y-slate-700 border-x-slate-700 font-bold	bg-[#121212] text-white"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+          <Input
+            placeholder="lastName"
+            className="border-y-slate-700 border-x-slate-700 font-bold bg-[#121212] text-white"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+          />
+          <Input
+            placeholder="username"
+            className="border-y-slate-700 border-x-slate-700 font-bold bg-[#121212] text-white"
+            value={username}
+            onChange={(e) => setUserName(e.target.value)}
+          />
+          <Input
+            placeholder="email"
+            className="border-y-slate-700 border-x-slate-700 font-bold	bg-[#121212] text-white"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Input
+            placeholder="password"
+            className="border-y-slate-700 border-x-slate-700 font-bold	bg-[#121212] text-white"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </CardContent>
+        <CardFooter className="w-screen">
+          <Button className="flex justify-center font-bold	bg-blue-700 w-[300px]">
+            Signup
+          </Button>
+        </CardFooter>
+        <div className="flex justify-center">
+          <div className="text-white">Have an account?</div>
+          <div className="text-blue-700 font-bold">Log in</div>
+        </div>
+      </Card>
+    </div>
+  );
+}
